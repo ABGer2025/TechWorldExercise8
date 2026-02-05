8c0524468b044cb5529b2bdf88f466820cda4326
ABGer2025 <abgabg2024@hotmail.com>
2026-02-05 07:53:40 +0100

    Update Jenkins shared library with files from GitLab repository

--- jenkins-shared-library/vars/buildDockerImage.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String imageName, String imageTag = 'latest') {
-    echo "Building Docker image: ${imageName}:${imageTag}"
-    sh "docker build -t ${imageName}:${imageTag} ."
-}
--- jenkins-shared-library/vars/deployToEnvironment.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String environment, String imageName, String imageTag) {
-    echo "Deploying ${imageName}:${imageTag} to ${environment}"
-    // Add deployment logic here
-    sh "echo 'Deploying to ${environment}...'"
-}
